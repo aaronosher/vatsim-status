53c9fd379eff7958749a49abff04cbe13104a61c
Aaron Osher <aaron@oshers.com>
2017-12-16 03:43:00 +0000

    Update gulpfile.js, package-lock.json, package.json, tsconfig.json
    Change Gulp, Babel, and TS Config to be more approriate

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,11 @@
 var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var tsify = require('tsify');
-var buffer = require('vinyl-buffer');
+var ts = require('gulp-typescript');  
+var babel = require('gulp-babel');
+var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task('default', function () {
-    return browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/index.ts'],
-        cache: {},
-        packageCache: {}
-    })
-    .plugin(tsify)
-    .transform('babelify', {
-        presets: ['env'],
-        extensions: ['.ts']
-    })
-    .bundle()
-    .pipe(source('index.js'))
-    .pipe(buffer())
-    .pipe(gulp.dest('dist'));
+gulp.task('default', function() {  
+    return tsProject.src()
+        .pipe(tsProject())
+        .pipe(babel())
+        .pipe(gulp.dest('dist'));
 });
